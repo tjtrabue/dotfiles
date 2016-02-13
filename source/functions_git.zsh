4ff0523b5233d4c1d3195f6c5f669e888fcf9633
tjtrabue <tom.trabue@gmail.com>
2016-02-13 11:12:56 -0500

    Turned gacp into its own binary since it was getting too big

--- source/functions_git.zsh
+++ source/functions_git.zsh
@@ -7,7 +7,7 @@
 ###########################################################################
 
 # Determines whether or not the directory is in a git repository
-function is-repo() {
+function isrepo() {
     if [[ $(git rev-parse --is-inside-work-tree) == "true" ]]; then
         return 0
     else
@@ -43,73 +43,6 @@ function ssh-gen() {
 #                              Staging/Pushing                            #
 #                                                                         #
 ###########################################################################
-
-# Commit and push changes upstream. Can optionally add files from a specified directory:
-function gacp () {
-        gacp_usage () {
-        echo "Add, commit, and push files to remote repository" 1>&2
-        echo "all in one command." 1>&2
-        echo "" 1>&2
-        echo "Usage:" 1>&2
-        echo "gacp [-m (message) -b (branch name) <file names>]" 1>&2
-        echo "" 1>&2
-        echo "Options:" 1>&2
-        echo "  -m <message> : the commit message (will be prompted for one if this arguemnet is omitted)" 1>&2
-        echo "  -b <remote branch> : the name of the remote branch to push changes to (\"master\" by default)" 1>&2
-    }
-
-    if [[ "$1" == "--help" || "$1" == "-h" ]]; then
-        gacp_usage
-        return 1
-    fi
-
-    if [[ `is-repo` == "false" ]]; then
-        echo "Not a git repository" 1>&2
-        echo "Aborting" 1>&2
-        return 1
-    fi
-
-    # Local variables
-    local commit_message=""
-    local remote_branch="master"
-
-    local OPTIND o
-        while getopts ":m:b:" o; do
-            case "${o}" in
-                m)
-                    commit_message="${OPTARG}"
-                    ;;
-                b)
-                    remote_branch="${OPTARG}"
-                    ;;
-                *)
-                    gacp_usage
-                    return
-                    ;;
-            esac
-        done
-    shift $((OPTIND-1))
-
-    # Add any files the user specifies:
-    for file in "$@"; do
-        git add "$file"
-    done
-
-    # Make sure there are files staged for commit
-    if [[ $(gadded) -eq 0 ]]; then
-        echo "No files staged for commit." 1>&2
-        return 1
-    fi
-
-    # Prompt for commit message:
-    while [[ "$commit_message" == "" ]]; do
-        echo "Type a commit message, then press ENTER:" 1>&2
-        read commit_message
-    done
-
-    git commit -m "$commit_message"
-    git push origin "$remote_branch"
-}
 
 
 ###########################################################################
@@ -201,11 +134,12 @@ function swged() {
 
 ###########################################################################
 ##                                                                       ##
-##                              Reverting                                ##
+##                          Reverting/Resetting                          ##
 ##                                                                       ##
 ###########################################################################
 
-function revertlast() {
+# Reverts the current repo to the state of its previous commit:
+function resetlast() {
     git rev-parse --is-inside-work-tree >> /dev/null
     if [[ $? -eq 0 ]]; then
         git reset --soft HEAD~1
