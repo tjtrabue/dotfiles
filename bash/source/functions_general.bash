83df4f06df37a7dfae9ee2725a44e471ace59fb2
tjtrabue <tom.trabue@gmail.com>
2016-03-02 00:01:13 -0500

    Fixed up atp

--- bash/source/functions_general.bash
+++ bash/source/functions_general.bash
@@ -49,9 +49,11 @@ shortpath () {
 # Makes an alias for the current working directory:
 diralias () {
     local short_path="`shortpath "$(pwd)"`";
-    sed -i "" "/export $1=/d" "$DIR_ALIAS_FILE";
-    echo "export $1=\"$short_path\"" >> "$DIR_ALIAS_FILE";
-    source ~/.dirs
+
+    sed -i 's:export\ $1=.*::g' "$DIR_ALIAS_FILE";
+    perl -pi -e 'chomp if eof' "$DIR_ALIAS_FILE" && echo >> "$DIR_ALIAS_FILE"
+    echo -n "export $1=\"$short_path\"" >> "$DIR_ALIAS_FILE";
+    source "$DIR_ALIAS_FILE"
 }
 
 
@@ -114,17 +116,19 @@ translate_dir_hist () {
 # an argument as well:
 atp () {
     if [[ "$#" -ne 1 ]]; then
-        local to_add=$(shortpath `pwd`);
+        local to_add=$(shortpath "$(pwd)");
     else
-        local to_add=$(shortpath `$1`);
+        local to_add="$1" && to_add=$(shortpath "$to_add");
     fi
 
-    echo $PATH | grep -ho "$to_add"
+    grep --color=never -ho "^export\ PATH=\$PATH:$to_add" ~/.path >> /dev/null
 
     if [ $? -ne 0 ]; then
         PATH=$PATH:$to_add
-        sed -i "" "s:^export::g" ~/.path
-        echo "export PATH=$PATH" >> ~/.path
+        sed -i "s|^export\ PATH=\$PATH:$to_add||g" ~/.path
+        sed -i "/^$/d" ~/.path
+        perl -pi -e 'chomp if eof' ~/.path && echo >> ~/.path
+        echo -n "export PATH=\$PATH:$to_add" >> ~/.path
         source ~/.path
         src
     else
