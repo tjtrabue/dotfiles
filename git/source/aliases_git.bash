81b190b5d861519f8bdb44b65941f5753daac6f3
tjtrabue <tom.trabue@gmail.com>
2016-04-07 23:35:37 -0400

    Took out git clean alias because the ucreset function basically does the same thing

--- git/source/aliases_git.bash
+++ git/source/aliases_git.bash
@@ -26,7 +26,6 @@ alias gcan="git commit --amend --no-edit"
 alias gacan="git add \"\$(git rev-parse --show-toplevel)\"/ && git commit --amend --no-edit"
 alias gp="git push"
 alias gpl="git pull"
-alias rmuntracked="git clean -d -ff"
 
 # Rebasing aliases:
 alias grb="git rebase"
