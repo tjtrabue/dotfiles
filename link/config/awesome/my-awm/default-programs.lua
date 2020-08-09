625cf6cb97c6412a71abaa1d9e120e6e1ac065bd
Tom Trabue <tom.trabue@gmail.com>
2020-08-09 15:44:43 -0400

    Ignoring some default cmus files

--- link/config/awesome/my-awm/default-programs.lua
+++ link/config/awesome/my-awm/default-programs.lua
@@ -4,6 +4,7 @@ local default_progs = {}
 
 default_progs.terminal = "alacritty"
 default_progs.browser = "firefox"
+default_progs.music = "cmus"
 default_progs.editor = os.getenv("EDITOR") or "vim"
 default_progs.editor_cmd = default_progs.terminal .. " -e " .. default_progs.editor
 default_progs.explorer = "xdg-open"
