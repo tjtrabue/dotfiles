a51b2ff789a45f850a5924d4e52631f2f70483c4
Tom Trabue <tom.trabue@gmail.com>
2020-08-16 17:28:35 -0400

    Added some pacman aliases

--- bash/source/linux/arch/aliases_arch.bash
+++ bash/source/linux/arch/aliases_arch.bash
@@ -4,4 +4,11 @@
 alias suspend="systemctl suspend"
 alias afk="xscreensaver-command -lock"
 
+# Pacman package manager
+alias supa="sudo pacman"
+alias supaq="sudo pacman -Q"
+alias supas="sudo pacman -S"
+alias supass="sudo pacman -Ss"
+alias supau="sudo pacman -Syyu"
+
 # vim:foldenable:foldmethod=marker:
