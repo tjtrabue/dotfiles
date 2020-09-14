f4c3f01bc914233c21cb6072ab299146d8c21f89
Tom Trabue <tom.trabue@gmail.com>
2020-09-14 19:23:35 -0400

    Added ruby aliases file

---
+++ bash/source/aliases_ruby.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# colorls aliases
+alias cls="colorls"
+alias cll="colorls -l --gs --sd"
+alias cla="colorls -lA --gs --sd"
+alias claa="colorls -la --gs --sd"
+
+# vim:foldenable:foldmethod=marker:
