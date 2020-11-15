362d96580405360f80f4b9f6130370018fe50fbb
Tom Trabue <tom.trabue@gmail.com>
2020-11-14 21:05:28 -0500

    Added xrandr init script

---
+++ copy/xrandr/multi_monitor.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# This script runs the xrandr utility to enable additional monitors.
+#
+# NOTE: You may need to adjust the arguments to --output, as the names of your
+# output devices may differ
+
+# Output (i.e., monitor) name variables
+main_monitor="eDP-1"
+second_monitor="DP-1-2-2"
+third_monitor="DP-1-2-3"
+
+# Configure the monitors
+xrandr --verbose --output "${main_monitor}" --auto --primary \
+  --output "${second_monitor}" --mode "2560x1440" --left-of "${main_monitor}" \
+  --output "${third_monitor}" --mode "2560x1440" --right-of "${main_monitor}"
+
+# xrandr --verbose --output eDP-1 --auto --primary \
+#   --output DP-1-2-2 --auto --left-of eDP-1 \
+#   --output DP-1-2-3 --auto --right-of eDP-1
