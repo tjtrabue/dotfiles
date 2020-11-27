5ff9ad53c740d14716e29a90f8a208d5e7b7bc78
Tom Trabue <tom.trabue@gmail.com>
2020-11-27 15:31:34 -0500

    Added some comments to aura aliases

--- bash/source/linux/arch/aliases_arch.bash
+++ bash/source/linux/arch/aliases_arch.bash
@@ -4,15 +4,21 @@
 alias suspend="systemctl suspend"
 alias afk="xscreensaver-command -lock"
 
-# Pacman package manager
+# Pacman package manager {{{
 alias pacq="sudo pacman -Q"
 alias pacs="sudo pacman -S"
 alias pacss="sudo pacman -Ss"
 alias pacsyu="sudo pacman -Syyu"
+# }}}
 
-# Aura AUR package manager
+# Aura AUR package manager {{{
+# Install an AUR package
 alias aurai="sudo aura -Aax"
+# Install an AUR package, allowing the PKGBUILD file to be edited before
+# installing.
 alias aurah="sudo aura -Aax --hotedit"
+# Upgrade all install AUR packages
 alias aurau="sudo aura -Aaxu --noconfirm"
+# }}}
 
 # vim:foldenable:foldmethod=marker:
