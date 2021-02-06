8c84531205091cabd7fc5b99e73abb93bdb17280
Tom Trabue <tom.trabue@gmail.com>
2021-02-06 10:28:28 -0500

    Added

--- bash/source/aliases_git.bash
+++ bash/source/aliases_git.bash
@@ -1,40 +1,40 @@
 #!/usr/bin/env bash
 
+### NOTE CONCERNING "MISSING" ALIASES:
+# Many of the most common git aliases are listed in functions_completion.bash,
+# which contains not only aliases, but completion directives that allow those
+# git command aliases to use bash-completion's awesome git completions. I
+# decided to list them in functions_completions.bash for cohesion, meaning that
+# I only have to look in one place if I want to change both the alias and its
+# completion function.
+
 # Branch alaises:
-alias gch="git checkout"
 alias newb="git checkout -b"
 alias delb="git branch -D"
 alias currb="git rev-parse --abbrev-ref HEAD"
 
 # Info aliases:
-alias gs="git status"
 alias groot="git rev-parse --show-toplevel"
 alias gtracked="git for-each-ref --format='%(upstream:short)' \"\$(git symbolic-ref -q HEAD)\""
 
 # Diff aliases
-alias gd="git diff"
 alias gdc="git diff --cached"
 
 # Return number of modified files:
 alias gadded="git diff --cached --numstat | wc -l | sed -e \"s/[ \t]//g\""
 
 # Staging/pushing/pulling aliases:
-alias ga="git add"
-alias gaa="git add \"\$(git rev-parse --show-toplevel)\"/"
-alias gc="git commit -m"
+alias gaa="git add -A"
 alias gca="git commit --amend"
 alias gcan="git commit --amend --no-edit"
 alias gacan="git add \"\$(git rev-parse --show-toplevel)\"/ && git commit --amend --no-edit"
-alias gp="git push"
 alias gpoh="git push origin HEAD"
-alias gpl="git pull"
 
 # Commit log aliases
 alias gl='git log --graph --color=always --format="%C(auto)%h%d %s %C(blue)%C(bold)%cr"'
 alias gll='git log --graph --color=always --format=full'
 
 # Rebasing aliases:
-alias grb="git rebase"
 alias grba="git rebase --abort"
 alias grbc="git rebase --continue"
 
--- bash/source/functions_completion.bash
+++ bash/source/functions_completion.bash
@@ -9,13 +9,52 @@ add_bash_completions() {
   # file.
   [ -f "${bashCompletionFile}" ] && . "${bashCompletionFile}"
 
+  # Add extra completions to special command aliases, such as `g` for `git`,
+  # and `r` for `docker`.
+  __add_git_alias_completions
+  __add_docker_alias_completions
+}
+
+# Add lots of cool completions for git aliases, such as `g` for `git`, and
+# `gco` for `git checkout`.
+__add_git_alias_completions() {
+  # Recognize `g` as an alias for `git`, thereby enabling git command line
+  # completions for `g`.
+  __add_completions_to_command_alias "git" "g"
+
+  # Add more specific git alias completions
+  if funcp "__git_complete"; then
+    alias ga='git add' && __git_complete ga _git_add
+    alias gb='git branch' && __git_complete gb _git_branch
+    alias gc='git commit' && __git_complete gc _git_commit
+    alias gcl='git clone' && __git_complete gcl _git_clone
+    alias gcle='git clean' && __git_complete gcle _git_clean
+    alias gco='git checkout' && __git_complete gco _git_checkout
+    alias gcon='git config' && __git_complete gcon _git_config
+    alias gd='git diff' && __git_complete gd _git_diff
+    alias gdes='git describe' && __git_complete gdes _git_describe
+    alias gm='git merge' && __git_complete gm _git_merge
+    alias gp='git push' && __git_complete gp _git_push
+    alias gpl='git pull' && __git_complete gpl _git_pull
+    alias grb='git rebase' && __git_complete grb _git_rebase
+    alias gre='git remote' && __git_complete gre _git_remote
+    alias gres='git reset' && __git_complete gres _git_reset
+    alias grest='git restore' && __git_complete grest _git_restore
+    alias grev='git revert' && __git_complete grev _git_revert
+    alias gs='git status' && __git_complete gs _git_status
+    alias gsb='git show-branch' && __git_complete gsb _git_show_branch
+    alias gst='git stash' && __git_complete gst _git_stash
+    alias gsw='git switch' && __git_complete gsw _git_switch
+    alias gt='git tag' && __git_complete gt _git_tag
+  fi
+}
+
+# Add lots of cool docker completions to aliased docker commands.
+__add_docker_alias_completions() {
   # Recognize `r` as an alias for `docker`, thereby enabling docker command line
   # completions for `r`.
-  # NOTE: The `d` alias is taken by `fasd` command line tool suite.
+  # NOTE: The `d` alias is already taken by `fasd` command line tool suite.
   __add_completions_to_command_alias "docker" "r"
-
-  # Same goes for `g` and `git`.
-  __add_completions_to_command_alias "git" "g"
 }
 
 # Enable shell completions for a command alias, such as `g` for `git`.
