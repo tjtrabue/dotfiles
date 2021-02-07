bdb3242435d3c95e94eecc339c236c591f639341
Tom Trabue <tom.trabue@gmail.com>
2021-02-07 16:10:21 -0500

    Refactored zsh dotfiles into new zdotdir folder

--- bash/source/functions_universal.bash
+++ bash/source/functions_universal.bash
@@ -430,10 +430,7 @@ v() {
   fi
 }
 
-# `tre` is a shorthand for `tree` with hidden files and color enabled, ignoring
-# the `.git` directory, listing directories first. The output gets piped into
-# `less` with options to preserve color and line numbers, unless the output is
-# small enough for one screen.
+# `tre` is a shorthand for `tree` with hidden files and color enabled.
 tre() {
   if [ -x "$(command -v colorls)" ]; then
     # Use fancy colorls tree command to show file tree, if we have colorls
---
+++ link/home/.zshenv
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+# Set the user's dotfile dir for Zsh, which will hold startup files for Zsh,
+# such as .zprofile, .zshrc, etc.
+export ZDOTDIR="${HOME}/.zsh"
+
+# vim:foldenable:foldmethod=marker:foldlevel=0
