caa116341c47212d4d46dd13fd4d4d167dc34b99
Tom Trabue <tom.trabue@gmail.com>
2021-03-08 12:23:06 -0500

    Added intelligence to awesome default programs

--- link/config/awesome/my-awm/default-programs.lua
+++ link/config/awesome/my-awm/default-programs.lua
@@ -2,8 +2,25 @@
 
 local default_progs = {}
 
-default_progs.terminal = "kitty"
-default_progs.browser = "firefox"
+-- Figure out web browser.
+if os.execute("command -v brave") then
+    -- Use Brave, which is like Chromium but better for privacy.
+    default_progs.browser = "brave"
+elseif os.execute("command -v chromium") then
+    -- Next best is Chromium.
+    default_progs.browser = "chromium"
+else
+    -- Default to Firefox (the traitorous thing...)
+    default_progs.browser = "firefox"
+end
+
+-- Figure out terminal program.
+if os.execute("command -v kitty") then
+    default_progs.terminal = "kitty"
+else
+    default_progs.terminal = "alacritty"
+end
+
 default_progs.music = "cmus"
 default_progs.editor = os.getenv("EDITOR") or "vim"
 default_progs.editor_cmd = default_progs.terminal .. " -e " .. default_progs.editor
