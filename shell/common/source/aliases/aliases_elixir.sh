7f62f604c24d217d79e645fa250bc08ce4c35eaa
Tom Trabue <tom.trabue@gmail.com>
2021-03-10 19:39:46 -0500

    Added elixir alias to start phoenix app interactively

--- shell/common/source/aliases/aliases_elixir.sh
+++ shell/common/source/aliases/aliases_elixir.sh
@@ -3,4 +3,7 @@
 # Start iex and load the current project's modules.
 alias iexm="iex -S mix"
 
+# Start Phoenix server interactively in iex.
+alias iphx="iex -S mix phx.server"
+
 # vim:foldenable:foldmethod=marker:foldlevel=0
