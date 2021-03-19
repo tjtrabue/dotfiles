9bb9843691008310d7928686c788cdae43e27483
Tom Trabue <tom.trabue@gmail.com>
2021-03-19 21:13:45 +0000

    cleaned up git functions file; removed a bunch of useless functions

--- shell/common/source/functions/functions_git.sh
+++ shell/common/source/functions/functions_git.sh
@@ -2,58 +2,25 @@
 # Contains custom Git functions that can be run in the terminal.
 
 # General {{{
-# Determines whether or not the current directory is in a git repository
+
+# Determines whether the current working directory is in a Git repository.
 isrepo() {
-  if git rev-parse --is-inside-work-tree >>/dev/null; then
-    return 0
-  fi
-  return 1
+  git rev-parse --is-inside-work-tree >>/dev/null 2>&1
 }
 
-# Retrieves the git url for the current repository
+# Retrieves the Git URL for the current repository.
 remoteurl() {
   git remote -v | egrep '^origin.*push' | awk '{print $2}'
 }
 
-# Returns the main remote branch for the repository:
+# Returns the main remote branch name for the current repository.
 mainbranch() {
   git remote show origin | grep 'HEAD branch' | awk '{print $3}'
 }
 
-# Opens the commit message for the current repo in the configured editor:
+# Opens the commit message for the current repo in the configured editor.
 emsg() {
   edit "$(git rev-parse --show-toplevel)/.git/COMMIT_EDITMSG"
-}
-
-# Checks out the commit for a given tag name and makes a branch for that tag:
-gcht() {
-  if [ -z "$1" ]; then
-    err "Must provide a tag name!"
-    return 1
-  fi
-  git checkout "tags/$1" -b "$1"
-}
-# }}}
-
-# Cloning {{{
-# Clones a repo and all submodules. Prompts user for username/email
-clone() {
-  if [ -n "$1" ]; then
-    echoe "Enter username for this project:"
-    read -r username
-    echoe "Now enter the email address for the project:"
-    read -r email
-
-    git clone --recursive "$1"
-    (
-      cd "$(basename "$1" | sed 's/\.git$//')"
-      git config user.name "$username"
-      git config user.email "$email"
-    )
-  else
-    err "No url entered"
-    return 1
-  fi
 }
 # }}}
 
@@ -108,40 +75,10 @@ upsubs() {
 }
 # }}}
 
-# Editor {{{
-# Changes the designated git editor.
-# Options are Sublime, Atom, and Vim:
-swged() {
-  if [ "$#" -ne 1 ]; then
-    echoe "No editor name supplied."
-    echo "Valid options are sublime, atom, and vim." 1>&2
-    return 1
-  fi
-
-  case "${1}" in
-  "sublime")
-    git config --global core.editor "subl -w"
-    ;;
-  "atom")
-    git config --global core.editor "atom --wait"
-    ;;
-  "vim")
-    git config --global core.editor "vim"
-    ;;
-  *)
-    echoe "Unknown operand $1."
-    echo "Please enter sublime, atom, or vim as an" 1>&2
-    echo "argument to this function." 1>&2
-    return 2
-    ;;
-  esac
-}
-# }}}
-
 # Reverting/resetting {{{
 # Reverts the current repo to the state of its previous commit:
 creset() {
-  if git rev-parse --is-inside-work-tree >>/dev/null; then
+  if git rev-parse --is-inside-work-tree >>/dev/null 2>&1; then
     git reset --soft HEAD~1
     git reset HEAD "$(git rev-parse --show-toplevel)"
   else
@@ -151,7 +88,7 @@ creset() {
 
 # Resets all uncommitted changes in the current repository
 ucreset() {
-  if git rev-parse --is-inside-work-tree >>/dev/null; then
+  if git rev-parse --is-inside-work-tree >>/dev/null 2>&1; then
     # Revert changes to modified files.
     git reset --hard
     # Remove all untracked files and directories.
@@ -164,13 +101,17 @@ ucreset() {
 # }}}
 
 # Committing {{{
+
+# Shortcut for `git commit -m '<msg>'`. No need to put quote marks around your
+# commit message. This function handles that bit for you.
 gcm() {
-  local message="$*"
+  local message="${*}"
 
   while [ -z "${message}" ]; do
     echoe "Enter commit message:"
     read -r message
   done
+
   git commit -m "${message}"
 }
 # }}}
