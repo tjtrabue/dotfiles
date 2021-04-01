009f51e4393c4fdcca583336e44074faaca0f8fc
Tom Trabue <tom.trabue@gmail.com>
2021-04-01 13:01:58 -0400

    Renamed sync_vars to varsync for convenience

--- shell/common/source/functions/functions_vars.sh
+++ shell/common/source/functions/functions_vars.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
 # Re-copy the .vars file from the dotfiles repository to the user's home
-# directory.
-sync_vars() {
+# directory. Handy when very few local changes to ~/.vars have been made, and
+# you want to gain access to new additions to the .vars file in the dotfiles
+# repo.
+#
+# This function makes a backup of the user's existing ~/.vars file
+# before copying the dotfiles .vars file so that the user may restore their
+# settings if need be.
+varsync() {
   local dotCopy="${DOTFILES_COPY:-${DOTFILES_HOME}/copy}"
   local dotVars="${dotCopy}/.vars"
   local userVars="${HOME}/.vars"
@@ -12,18 +18,18 @@ sync_vars() {
     return 1
   fi
 
-  __backup_user_vars_file
+  __backup_user_vars_file "${userVars}"
 
   log_info "Copying ${GREEN}${dotVars}${NC} to ${GREEN}${userVars}${NC}"
   cp -f "${dotVars}" "${userVars}"
 
-  __add_extra_os_vars
+  __add_extra_os_vars "${userVars}"
 }
 
 # Make a backup of the ~/.vars file before re-copying the dotfiles .vars file
 # to ~/.vars
 __backup_user_vars_file() {
-  local userVars="${HOME}/.vars"
+  local userVars="${1:-${HOME}/.vars}"
 
   if [ -f "${userVars}" ]; then
     log_info "Backing up user's ${userVars} file"
@@ -33,10 +39,10 @@ __backup_user_vars_file() {
 
 # Inject extra environment variables into the ~/.vars file, if prudent.
 __add_extra_os_vars() {
+  local userVars="${1:-${HOME}/.vars}"
   local os="$(getosinfo | head -1 | sed 's/Distribution:\s*//')"
   local dotCopy="${DOTFILES_COPY:-${DOTFILES_HOME}/copy}"
   local extraVarsLinuxDir="${dotCopy}/var_files/linux"
-  local userVars="${HOME}/.vars"
   local markerString="#<additional-vars-insert>"
   local extraVarsFile="NONE"
 
