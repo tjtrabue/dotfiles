0276929a797cfa28151b1b27a8c965e593b6110c
Tom Trabue <tom.trabue@gmail.com>
2021-07-13 19:27:32 -0400

    Added window library to emacs

---
+++ link/emacs/require/my-window.el
@@ -0,0 +1,48 @@
+;;; my-window.el --- summary -*- lexical-binding: t -*-
+
+;; Author: Thomas Jacob Trabue
+;; Maintainer: Thomas Jacob Trabue
+;; Version: version
+;; Package-Requires: ()
+;; Homepage: homepage
+;; Keywords: keywords
+
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; commentary
+
+;;; Code:
+
+;;;###autoload
+(defun my-window-toggle-frame-split ()
+  "If the frame is split vertically, split it horizontally or vice versa.
+Assumes that the frame is only split into two."
+  (interactive)
+  (unless (= (length (window-list)) 2) (error "Can only toggle a frame split in two"))
+  (let ((split-vertically-p (window-combined-p)))
+    (delete-window) ; closes current window
+    (if split-vertically-p
+        (split-window-horizontally)
+      (split-window-vertically)) ; gives us a split with the other window twice
+    (switch-to-buffer nil))) ; restore the original window in this part of the frame
+
+(provide 'my-window)
+
+;;; my-window.el ends here
