852bf3e8ffc03e1ff117bb567e0a90045f30f0fd
Tom Trabue <tom.trabue@gmail.com>
2021-07-18 12:20:56 -0400

    Added function grepedit for editing files containing a pattern of test

--- shell/common/source/functions/functions_editor.sh
+++ shell/common/source/functions/functions_editor.sh
@@ -22,6 +22,39 @@ ctrlp() {
   fi
 }
 
+# Open a number of files containing text matching an input pattern in the
+# configured $EDITOR program.
+# Optionally takes a second string of CLI options for the search command.
+grepedit() {
+  local searchCommand="grep -l"
+  # The regex to search files for.
+  local searchPattern="${1}"
+  # Extra CLI options for the search command (not required).
+  local extraOpts="${2}"
+
+  if [ -z "${searchPattern}" ]; then
+    err "No search pattern provided to patedit."
+    return 1
+  fi
+
+  if [ -x "$(command -v rg)" ]; then
+    # Use ripgrep if available.
+    searchCommand="rg -l"
+  elif [ -x "$(command -v ag)" ]; then
+    # In lieu of ripgrep, the_silver_searcher is still better than grep.
+    searchCommand="ag -l"
+  fi
+
+  # Add extra CLI args to search command if they were provided.
+  [ -n "${extraOpts}" ] && searchCommand="${searchCommand} ${extraOpts}"
+
+  # Add the search pattern to the end of the search command.
+  searchCommand="${searchCommand} -- ${searchPattern}"
+
+  # Open the files in the configured editor
+  eval "edit "$(eval "${searchCommand}")""
+}
+
 # Open a "source" file in the configured editor.
 esf() {
   pushd "$DOTFILES_HOME/bash/source" &>/dev/null
