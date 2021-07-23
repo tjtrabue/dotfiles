01deffc96a5a3db771167d89d541a02df473a738
Tom Trabue <tom.trabue@gmail.com>
2021-07-23 15:40:47 -0400

    Added more Clojure modes to list of LSP enabled modes

--- link/emacs/require/my-custom-lsp.el
+++ link/emacs/require/my-custom-lsp.el
@@ -49,6 +49,8 @@ for any programming language that supports a formatting tool.")
                                       c++-mode
                                       c-mode
                                       clojure-mode
+                                      clojurec-mode
+                                      clojurescript-mode
                                       cmake-mode
                                       cperl-mode
                                       css-mode
