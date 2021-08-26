a1e6e4d8ce2ceb5a56536eee330d82e1162965ed
Tom Trabue <tom.trabue@gmail.com>
2021-08-26 11:01:46 +0200

    Added functions for initializing helm completions for shells

---
+++ shell/common/source/functions/functions_kubernetes.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Add shell completion for helm.
+init_helm_completions() {
+  local userShell="$(basename "${SHELL}")"
+
+  case "${userShell}" in
+    "bash")
+      __init_helm_completions_bash
+      ;;
+    "zsh")
+      __init_helm_completions_zsh
+      ;;
+  esac
+}
+
+__init_helm_completions_bash() {
+  local bashCompletionDir="${HOME}/.bash_completion.d"
+  local helmCompletionFile="${bashCompletionDir}/helm"
+
+  log_info "Initializing helm completions for bash"
+
+  mkdir -p "${bashCompletionDir}"
+  helm completion bash >"${helmCompletionFile}"
+  soure <(helm completion bash)
+}
+
+__init_helm_completions_zsh() {
+  log_info "Initializing helm completions for zsh"
+  helm completion zsh >"${fpath[1]}/_helm"
+  source <(helm completion zsh)
+}
+
+# vim:foldenable:foldmethod=indent:foldnestmax=1
