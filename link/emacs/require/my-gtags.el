75cb7509571e66e2211a168aa2fe5c64af97db77
Tom Trabue <tom.trabue@gmail.com>
2021-09-02 10:42:45 +0200

    Refactored some functions from my-init to new files

---
+++ link/emacs/require/my-gtags.el
@@ -0,0 +1,51 @@
+;;; my-gtags.el --- summary -*- lexical-binding: t -*-
+
+;; Author: Thomas Jacob Trabue
+;; Maintainer: Thomas Jacob Trabue
+;; Version: version
+;; Package-Requires: (dependencies)
+;; Homepage: homepage
+;; Keywords: keywords
+
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; commentary
+
+;;; Code:
+
+(defun my-gtags-root-dir ()
+  "Returns GTAGS root directory or nil if doesn't exist."
+  (with-temp-buffer
+    (if (zerop (call-process "global" nil t nil "-pr"))
+      (buffer-substring (point-min) (1- (point-max)))
+      nil)))
+
+(defun my-gtags-update ()
+  "Make GTAGS incremental update"
+  (call-process "global" nil nil nil "-u"))
+
+(defun my-gtags-update-hook-fn ()
+  "Update GTAGS file whenever an appropriate file is saved."
+  (when (my-gtags-root-dir)
+    (my-gtags-update)))
+
+(provide 'my-gtags)
+
+;;; my-gtags.el ends here
---
+++ link/emacs/require/my-straight-helpers.el
@@ -0,0 +1,51 @@
+;;; my-straight-helpers.el --- summary -*- lexical-binding: t -*-
+
+;; Author: Thomas Jacob Trabue
+;; Maintainer: Thomas Jacob Trabue
+;; Version: version
+;; Package-Requires: (dependencies)
+;; Homepage: homepage
+;; Keywords: keywords
+
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; Convenience functions that leverage the straight.el package manager.
+
+;;; Code:
+
+(defun my-straight-helpers-update-and-freeze ()
+  "Custom function that updates all installed packages and regenerates the
+    lock file."
+  (interactive)
+  (straight-pull-all)
+  (straight-rebuild-all)
+  (straight-freeze-versions t))
+
+(defun my-straight-helpers-pull-recipe-repositories ()
+  "Update all straight.el recipe repositories. This is a custom function that
+        I defined in order to make my life easier.
+        --tjtrabue"
+  (interactive)
+  (dolist (repo straight-recipe-repositories)
+    (straight-pull-package repo)))
+
+(provide 'my-straight-helpers)
+
+;;; my-straight-helpers.el ends here
