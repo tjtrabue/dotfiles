d4b2dd3a2dd237bb318928ea0e83e24988a7e747
Tom Trabue <tom.trabue@gmail.com>
2021-09-04 17:45:56 -0400

    Fixed more lua-language-server neovim config

--- link/config/nvim/plug-config/lsp.lua
+++ link/config/nvim/plug-config/lsp.lua
@@ -233,11 +233,17 @@ require "lspconfig".sumneko_lua.setup {
       },
       workspace = {
         -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true)
+        library = vim.api.nvim_get_runtime_file("", true),
+        -- Neovim does not currently support third party tools
+        checkThirdParty = false
       },
       -- Do not send telemetry data containing a randomized but unique identifier
       telemetry = {
         enable = false
+      },
+      -- Enable code hints
+      hint = {
+        enable = true
       }
     }
   }
