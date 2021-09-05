d30250f31db13cfc23ccd89fa89118878557c46e
Tom Trabue <tom.trabue@gmail.com>
2021-09-05 17:58:05 -0400

    Made vim-easymotion more usable

--- link/config/nvim/lua/plugins.lua
+++ link/config/nvim/lua/plugins.lua
@@ -225,6 +225,30 @@ packer.startup(
         {"RishabhRD/popfix"}
       }
     }
+    -- Provides the missing "LspInstall" function for nvim-lsp.
+    use {
+      "kabouzeid/nvim-lspinstall",
+      requires = {
+        {"neovim/nvim-lspconfig"}
+      },
+      config = [[
+        local function setup_servers()
+          require'lspinstall'.setup()
+          local servers = require'lspinstall'.installed_servers()
+          for _, server in pairs(servers) do
+            require'lspconfig'[server].setup{}
+          end
+        end
+
+        setup_servers()
+
+        -- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
+        require'lspinstall'.post_install_hook = function ()
+          setup_servers() -- reload installed servers
+          vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
+        end
+      ]]
+    }
     -- Patches colorschemes that don't support LSP client syntax for
     -- diagnostics.
     use "folke/lsp-colors.nvim"
--- link/config/nvim/plug-config/vim-easymotion.vim
+++ link/config/nvim/plug-config/vim-easymotion.vim
@@ -2,16 +2,24 @@
 " They only work in Normal mode, though.
 
 " <Leader>f{char} to move to {char}
-map  <leader><leader>f <Plug>(easymotion-bd-f)
-nmap <leader><leader>f <Plug>(easymotion-overwin-f)
+map  <leader>f <Plug>(easymotion-bd-f)
+nmap <leader>f <Plug>(easymotion-overwin-f)
 
 " s{char}{char} to move to {char}{char}
 nmap s <Plug>(easymotion-overwin-f2)
 
 " Move to line
-map <leader><leader>L <Plug>(easymotion-bd-jk)
-nmap <leader><leader>L <Plug>(easymotion-overwin-line)
+map <leader>L <Plug>(easymotion-bd-jk)
+nmap <leader>L <Plug>(easymotion-overwin-line)
 
 " Move to word
-map  <leader><leader>w <Plug>(easymotion-bd-w)
-nmap <leader><leader>w <Plug>(easymotion-overwin-w)
+map  <leader>w <Plug>(easymotion-bd-w)
+nmap <leader>w <Plug>(easymotion-overwin-w)
+
+" Remap 'f' and 'F' in normal mode to be more useful.
+" Move to {char}
+map f <Plug>(easymotion-bd-f)
+nmap f <Plug>(easymotion-overwin-f)
+" Move to word
+map F <Plug>(easymotion-bd-w)
+nmap F <Plug>(easymotion-overwin-w)
