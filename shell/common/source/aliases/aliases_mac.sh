5b097db6bd2817dbdb9ae7a408582cffc5883ffe
Tom Trabue <tom.trabue@gmail.com>
2021-09-18 11:16:00 -0400

    Added aliases file for macos

---
+++ shell/common/source/aliases/aliases_mac.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Homebrew {{{
+
+# Update formulae and then upgrade them in one fell swoop.
+alias brewup="brew update && brew upgrade"
+# }}}
+
+# vim:foldenable:foldmethod=marker:foldlevel=0
