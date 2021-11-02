2db20944f3122323bcfc40963d84bea7f53ea372
Tom Trabue <tom.trabue@gmail.com>
2021-11-02 11:40:07 -0500

    Re-added lspkind after fix was submitted

--- link/config/nvim/plug-config/nvim-cmp.lua
+++ link/config/nvim/plug-config/nvim-cmp.lua
@@ -8,16 +8,17 @@ local lspkind = require "lspkind"
 
 cmp.setup(
   {
-    -- This plugin is currently broken. Wait for fix.
-    --[[ formatting = {
+    formatting = {
       -- Use VSCode-like pictograms in completions
       format = lspkind.cmp_format({with_text = false, maxwidth = 50})
-    }, ]]
+    },
     snippet = {
       expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        -- Choose your snippets provider. vsnip will be awesome once it gets
+        -- more snippets in the friendly-snippets repo.
+        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
         -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+        vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
         -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
       end
     },
@@ -43,9 +44,9 @@ cmp.setup(
     sources = cmp.config.sources(
       {
         {name = "nvim_lsp"},
-        {name = "vsnip"} -- For vsnip users.
+        -- {name = "vsnip"} -- For vsnip users.
         -- { name = 'luasnip' }, -- For luasnip users.
-        -- { name = 'ultisnips' }, -- For ultisnips users.
+        {name = "ultisnips"} -- For ultisnips users.
         -- { name = 'snippy' }, -- For snippy users.
       },
       {
