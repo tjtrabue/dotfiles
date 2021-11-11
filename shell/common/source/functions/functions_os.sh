4d8d61b7789de8dabe066cb6574c96803d2775aa
Tom Trabue <tom.trabue@gmail.com>
2021-11-11 10:40:36 -0600

    Working on os info functions

--- shell/common/source/functions/functions_os.sh
+++ shell/common/source/functions/functions_os.sh
@@ -3,29 +3,29 @@
 # Gets the Linux OS's name and version.
 getosinfo() {
   # Figure out the distro based on one of a few things.
-  if [ -f :/etc/os-release ]; then
+  if [ -f "/etc/os-release" ]; then
     # freedesktop.org and systemd
     source "/etc/os-release"
-    OS="$NAME"
+    OS="${NAME}"
     VER="${VERSION_ID:-${BUILD_ID}}"
   elif type lsb_release >>/dev/null 2>&1; then
     # linuxbase.org
     OS="$(lsb_release -si)"
     VER="$(lsb_release -sr)"
-  elif [ -f /etc/lsb-release ]; then
+  elif [ -f "/etc/lsb-release" ]; then
     # For some versions of Debian/Ubuntu without lsb_release command
-    source /etc/lsb-release
-    OS="$DISTRIB_ID"
-    VER="$DISTRIB_RELEASE"
-  elif [ -f /etc/debian_version ]; then
+    source "/etc/lsb-release"
+    OS="${DISTRIB_ID}"
+    VER="${DISTRIB_RELEASE}"
+  elif [ -f "/etc/debian_version" ]; then
     # Older Debian/Ubuntu/etc.
     OS="Debian"
-    VER="$(cat /etc/debian_version)"
-  elif [ -f /etc/SuSe-release ]; then
+    VER="$(cat "/etc/debian_version")"
+  elif [ -f "/etc/SuSe-release" ]; then
     # Older SuSE/etc.
     OS="SuSE"
     VER="unknown"
-  elif [ -f /etc/redhat-release ]; then
+  elif [ -f "/etc/redhat-release" ]; then
     # Older Red Hat, CentOS, etc.
     OS="RedHat"
     VER="unknown"
@@ -35,8 +35,7 @@ getosinfo() {
   fi
 
   # Return the distribution name and version.
-  echo "Distribution: ${OS}"
-  echo "Version: ${VER}"
+  printf "%s: %s\n%s: %s\n" "Distribution" "${OS}" "Version" "${VER}"
 }
 
 # Retrieves the system package manager for the current operating system.
@@ -45,37 +44,39 @@ getpm() {
   local pm=""
 
   # Get thr distribution ID:
-  distId="$(
-    getdistro |
-    grep '^Distribution:' |
-    sed 's/^Distribution:\s*//'
-  )"
+  distId="$(getdistro)"
 
-  case "$distId" in
+  case "${distId}" in
+    "Arch Linux")
+      pm="pacman"
+      ;;
     "CentOS" | "RedHat")
       pm="yum"
+      ;;
+    "Darwin")
+      pm="brew"
       ;;
     "Ubuntu")
       pm="apt-get"
       ;;
     *)
-      err "Unknown OS $distId"
+      err "Unknown OS: ${BLUE}${distId}${NC}"
       return 1
       ;;
   esac
 
   # Return the name of the package manager:
-  echo "$pm"
+  printf "%s\n" "${pm}"
 }
 
 # Gets the Linux distribution name only
 getdistro() {
-  getosinfo | grep 'Distribution:' | sed 's/Distribution: //'
+  getosinfo | grep 'Distribution:' | sed -E 's/^\s*Distribution:\s*//'
 }
 
 # Gets the Linux distrubtion version only
 getosversion() {
-  getosinfo | grep 'Version:' | sed 's/Version: //'
+  getosinfo | grep 'Version:' | sed -E 's/^\s*Version:\s*//'
 }
 
 # vim:foldenable:foldmethod=indent::foldnestmax=1
