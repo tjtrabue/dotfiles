4878382c713d5e4cd10b6fa90d1dbd943fb13a10
Tom Trabue <tom.trabue@gmail.com>
2021-11-15 10:33:50 -0600

    Enhanced flutter installer function

--- shell/common/source/functions/functions_dart.sh
+++ shell/common/source/functions/functions_dart.sh
@@ -3,19 +3,25 @@
 # Install the Flutter SDK to a predictable location on the file system.
 install_or_update_flutter_sdk() {
   local gitUrl="https://github.com/flutter/flutter.git"
-  local branch="stable"
+  local flutterSdkBranch="stable"
   local destDir="${WS:-${HOME}/workspace}/$(basename "${gitUrl%.git}")"
 
   if [ -d "${destDir}" ]; then
     log_info "Updating Flutter SDK"
     git -C "${destDir}" reset --hard
     git -C "${destDir}" clean -df
-    git -C "${destDir}" checkout "${branch}"
+    git -C "${destDir}" checkout "${flutterSdkBranch}"
     git -C "${destDir}" pull
   else
     log_info "Installing Flutter SDK to: ${BLUE}${destDir}${NC}"
-    git clone -b "${branch}" "${gitUrl}" "${destDir}"
+    git clone -b "${flutterSdkBranch}" "${gitUrl}" "${destDir}"
   fi
+
+  # Make sure Flutter's binaries are on PATH.
+  atp "${destDir}/bin"
+
+  # Download additional dependencies for Flutter and print diagnostic output.
+  "${destDir}/bin/flutter" doctor
 }
 
 # vim:foldenable:foldmethod=indent:foldnestmax=1
