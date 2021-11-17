6cb0796ca2b0c28e5785f408cc1acf9367b34b88
Tom Trabue <tom.trabue@gmail.com>
2021-11-16 20:16:26 -0600

    smoothed out python installer function

--- shell/common/source/functions/functions_python.sh
+++ shell/common/source/functions/functions_python.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+# Prepare Python environment for the current shell.
+src_python_for_profile() {
+  local pyenvHome="${PYENV_ROOT:-${HOME}/.pyenv}"
+
+  if ! __tool_installed "pyenv" "${pyenvHome}"; then
+    install_or_update_pyenv
+    install_latest_python
+  fi
+
+  if [ "$(command -v pyenv)" != "" ]; then
+    # Add shims to PATH
+    eval "$(pyenv init --path)"
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+  fi
+}
+
 # Installs all packages listed in the pyhon2 and python3 package lists
 install_python_packages() {
   # NOTE: Python 2.x has reached its end-of-life, and is no longer supported.
@@ -9,6 +26,59 @@ install_python_packages() {
   if __python3_pip_installed; then
     __install_python3_packages
   fi
+}
+
+# Update all installed Python packages.
+# Handles Python 2.X and Python 3.Y.
+update_python_packages() {
+  # NOTE: Python 2.x has reached its end-of-life, and is no longer supported.
+  if __python2_pip_installed; then
+    __update_python2_packages
+  fi
+  if __python3_pip_installed; then
+    __update_python3_packages
+  fi
+}
+
+# Install or update pyenv for managing python versions.
+install_or_update_pyenv() {
+  local pyenvDir="${PYENV_DIR:-${HOME}/.pyenv}"
+  if [ -d "${pyenvDir}" ]; then
+    __update_pyenv
+  else
+    __install_pyenv
+  fi
+}
+
+# Install the latest version of Python with pyenv.
+install_latest_python() {
+  local latestPythonVersion="$(pyenv install --list |
+  sed 's/^\s*//' | grep '^3' | tail -1)"
+
+  log_info "Installing ${YELLOW}Python ${latestPythonVersion}${NC} with pyenv"
+  pyenv install -s "${latestPythonVersion}" &&
+  pyenv global "${latestPythonVersion}"
+}
+
+get_python2_version() {
+  python2 --version 2>&1 | awk '{print $2}'
+}
+
+get_python3_version() {
+  python3 --version 2>&1 | awk '{print $2}'
+}
+
+# Install pyenv for managing python environments.
+__install_pyenv() {
+  local pyenvHome="${PYENV_DIR:-${HOME}/.pyenv}"
+
+  __install_tool_from_url_and_script "pyenv" "${pyenvHome}" \
+    "https://pyenv.run"
+}
+
+__update_pyenv() {
+  log_info "Updating pyenv to latest version"
+  pyenv update
 }
 
 __install_python2_packages() {
@@ -28,18 +98,6 @@ __install_python3_packages() {
   else
     err "Could not find Python 3 package file."
     return 1
-  fi
-}
-
-# Update all installed Python packages.
-# Handles Python 2.X and Python 3.Y.
-update_python_packages() {
-  # NOTE: Python 2.x has reached its end-of-life, and is no longer supported.
-  if __python2_pip_installed; then
-    __update_python2_packages
-  fi
-  if __python3_pip_installed; then
-    __update_python3_packages
   fi
 }
 
@@ -63,63 +121,6 @@ __python2_pip_installed() {
 # Return 0 if PIP installed for Python 3.Y
 __python3_pip_installed() {
   python3 -m pip --version >>/dev/null 2>&1
-}
-
-# Install or update pyenv for managing python versions.
-install_or_update_pyenv() {
-  local pyenvDir="${PYENV_DIR:-${HOME}/.pyenv}"
-  if [ -d "${pyenvDir}" ]; then
-    __update_pyenv
-  else
-    __install_pyenv
-  fi
-}
-
-# Install the latest version of Python with pyenv.
-install_latest_python() {
-  local latestPythonVersion="$(pyenv install --list |
-  sed 's/^\s*//' | grep '^3' | tail -1)"
-
-  pyenv install "${latestPythonVersion}"
-  pyenv global "${latestPythonVersion}"
-}
-
-# Prepare Python environment for the current shell.
-src_python_for_profile() {
-  local pyenvHome="${PYENV_ROOT:-${HOME}/.pyenv}"
-
-  if ! __tool_installed "pyenv" "${pyenvHome}"; then
-    install_or_update_pyenv
-    install_latest_python
-  fi
-
-  if [ "$(command -v pyenv)" != "" ]; then
-    # Add shims to PATH
-    eval "$(pyenv init --path)"
-    eval "$(pyenv init -)"
-    eval "$(pyenv virtualenv-init -)"
-  fi
-}
-
-get_python2_version() {
-  python2 --version 2>&1 | awk '{print $2}'
-}
-
-get_python3_version() {
-  python3 --version 2>&1 | awk '{print $2}'
-}
-
-# Install pyenv for managing python environments.
-__install_pyenv() {
-  local pyenvHome="${PYENV_DIR:-${HOME}/.pyenv}"
-
-  __install_tool_from_url_and_script "pyenv" "${pyenvHome}" \
-    "https://pyenv.run"
-}
-
-__update_pyenv() {
-  log_info "Updating pyenv to latest version"
-  pyenv update
 }
 
 # vim:foldenable:foldmethod=indent::foldnestmax=1
