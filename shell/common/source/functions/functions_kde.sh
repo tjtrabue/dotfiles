acc5a9df1548272d15a285f41d9e9449f7e28b01
Tom Trabue <tom.trabue@gmail.com>
2021-11-25 14:27:26 -0500

    Small update to kwin tiling installer

--- shell/common/source/functions/functions_kde.sh
+++ shell/common/source/functions/functions_kde.sh
@@ -6,16 +6,24 @@ init_kde() {
 }
 
 install_kwin_tiling() {
-  __clone_kwin_tiling_repo
+  __clone_or_update_kwin_tiling_repo
   __install_kwin_tiling_script
 }
 
-__clone_kwin_tiling_repo() {
+__clone_or_update_kwin_tiling_repo() {
   local repoUrl="https://github.com/kwin-scripts/kwin-tiling.git"
   local destDir="${WS}/$(basename "${repoUrl%.git}")"
 
-  log_info "Cloning KWin Tiling repository to: ${BLUE}${destDir}${NC}"
-  git clone "${repoUrl}" "${destDir}"
+  if [ -d "${destDir}" ]; then
+    log_info "Updating KWin Tiling repo"
+    git -C "${destDir}" clean -df
+    git -C "${destDir}" reset --hard
+    git -C "${destDir}" checkout "$(defaultbranch "${destDir}")"
+    git -C "${destDir}" pull
+  else
+    log_info "Cloning KWin Tiling repository to: ${BLUE}${destDir}${NC}"
+    git clone "${repoUrl}" "${destDir}"
+  fi
 }
 
 __install_kwin_tiling_script() {
