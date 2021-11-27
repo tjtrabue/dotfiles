4b7a73c849a419f16589ec70072565757a7ad676
Tom Trabue <tom.trabue@gmail.com>
2021-11-26 23:00:06 -0600

    Added generic function for installing nerd fonts

--- shell/common/source/functions/functions_font.sh
+++ shell/common/source/functions/functions_font.sh
@@ -3,7 +3,38 @@
 # Locate a given font via an input regular expression
 findfont() {
   local pattern="$1"
-  eval "fc-list | grep -E -i '$pattern' | less"
+
+  eval "fc-list | grep -E -i '${pattern}' | less"
+}
+
+# Install or update the amazing Nerd Fonts repository.
+sync_nerd_fonts() {
+  local nerdFontsDestDir="${WS}/nerd-fonts"
+
+  __clone_fresh_nerd_fonts_repo
+
+  if [ -d "${nerdFontsDestDir}" ]; then
+    (
+      cd "${nerdFontsDestDir}"
+      ./install.sh
+    )
+  else
+    err "Nerd fonts directory does not exist."
+    return 1
+  fi
+}
+
+__clone_fresh_nerd_fonts_repo() {
+  local nerdFontsGitUrl="https://github.com/ryanoasis/nerd-fonts.git"
+  local nerdFontsDestDir="${WS}/nerd-fonts"
+
+  if [ -d "${nerdFontsDestDir}" ]; then
+    log_info "Removing old Nerd Fonts repository"
+    rm -rf "${nerdFontsDestDir}"
+  fi
+
+  log_info "Cloning Nerd Fonts repository to: ${BLUE}${nerdFontsDestDir}${NC}"
+  git clone --depth 1 "${nerdFontsGitUrl}" "${nerdFontsDestDir}"
 }
 
 # vim:foldenable:foldmethod=indent::foldnestmax=1
