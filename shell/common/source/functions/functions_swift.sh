8c11f1ddfb2c0594a317d00267d3acb9c608dbfb
Tom Trabue <tom.trabue@gmail.com>
2021-11-30 17:19:36 -0600

    Added functions to install swift tools

---
+++ shell/common/source/functions/functions_swift.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+
+# Install or update the fantastic and powerful swift-format tool.
+# Requires a full swift installation.
+install_or_update_swift_format() {
+  local swiftFormatDir="${WS}/swift-format"
+
+  if [ -d "${swiftFormatDir}" ]; then
+    __update_swift_format "${swiftFormatDir}"
+  else
+    __install_swift_format "${swiftFormatDir}"
+  fi
+}
+
+# Print the semantic version of our Swift installation.
+get_swift_version() {
+  swift -version 2>&1 | grep -i -o 'Apple Swift version [0-9]\.[0-9]\.[0-9]' |
+    awk '{print $4}'
+}
+
+__update_swift_format() {
+  local swiftFormatDir="${1:-${WS}/swift-format}"
+
+  git -C "${swiftFormatDir}" clean -df
+  git -C "${swiftFormatDir}" reset --hard
+  git -C "${swiftFormatDir}" fetch
+}
+
+__install_swift_format() {
+  local swiftFormatDir="${1:-${WS}/swift-format}"
+  local swiftFormatGitUrl="https://github.com/apple/swift-format.git"
+  local versionRef="swift-5.5-branch"
+
+  __clone_swift_format "${swiftFormatDir}"
+  __build_swift_format "${swiftFormatDir}"
+}
+
+__clone_swift_format() {
+  local destDir="${1:-${WS}/swift-format}"
+  local swiftFormatGitUrl="https://github.com/apple/swift-format.git"
+
+  if [ ! -d "${destDir}" ]; then
+    log_info "Cloning swift-format GitHub repository"
+    git clone "${swiftFormatGitUrl}" "${destDir}"
+  else
+    warn "swift-format directory already exists at: ${BLUE}${destDir}${NC}"
+  fi
+}
+
+# Compile and install the swift-format tool.
+__build_swift_format() {
+  local swiftFormatDir="${1:-${WS}/swift-format}"
+
+  __checkout_correct_swift_format_ref "${swiftFormatDir}"
+
+  log_info "Installing swift-format from ref: ${versionRef}"
+  (
+    cd "${swiftFormatDir}"
+    swift build -c release
+  )
+}
+
+# We need to build the correct version of swift-format for our current Swift
+# installation.
+__checkout_correct_swift_format_ref() {
+  local swiftFormatDir="${1:-${WS}/swift-format}"
+  local swiftMajorMinorVersion
+  local versionRef
+
+  swiftMajorMinorVersion="$(get_swift_version | grep -o '^[0-9]\.[0-9]')"
+  versionRef="$(git -C "${swiftFormatDir}" \
+    branch -r --color=never --format="%(refname:lstrip=-1)" |
+    grep "${swiftMajorMinorVersion}")"
+
+  if [ -n "${versionRef}" ]; then
+    log_info "Checking out swift-format ref: ${MAGENTA}${versionRef}${NC}"
+    git -C "${swiftFormatDir}" checkout "${versionRef}"
+  else
+    err "Could not determine correct swift-format version ref"
+    return 1
+  fi
+}
+
+# vim:foldenable:foldmethod=indent:foldnestmax=1
