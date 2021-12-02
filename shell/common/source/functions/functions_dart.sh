73a40818b27810a5fdd35fa47bf7798588eef56f
Tom Trabue <tom.trabue@gmail.com>
2021-12-02 16:39:49 -0600

    Added more dart funcs

--- shell/common/source/functions/functions_dart.sh
+++ shell/common/source/functions/functions_dart.sh
@@ -2,9 +2,9 @@
 
 # Pull Dart configuration into the current shell profile.
 src_dart_for_profile() {
-  local dvmHome="${HOME}/.dvm"
+  local dvmRoot="${DVM_ROOT:-${HOME}/.dvm}"
 
-  if [ ! -d "${dvmHome}" ]; then
+  if [ ! -d "${dvmRoot}" ]; then
     install_or_update_dvm
   fi
 
@@ -13,33 +13,60 @@ src_dart_for_profile() {
 
 # Install or update the dart version manager (dvm).
 install_or_update_dvm() {
-  local dvmHome="${HOME}/.dvm"
+  local dvmRoot="${DVM_ROOT:-${HOME}/.dvm}"
 
-  if [ -d "${dvmHome}" ]; then
+  if [ -d "${dvmRoot}" ]; then
     __update_dvm
   else
     __install_dvm
   fi
 }
 
+install_latest_dart() {
+  local dvmRoot="${DVM_ROOT:-${HOME}/.dvm}"
+
+  if [ ! -d "${dvmRoot}" ]; then
+    __install_dvm
+  fi
+
+  __src_dvm_for_profile
+  dvm install latest --default
+  __add_latest_dart_to_path
+}
+
+__add_latest_dart_to_path() {
+  local dvmRoot="${DVM_ROOT:-${HOME}/.dvm}"
+  local dvmDefaultEnv="${dvmRoot}/environments/default"
+  local latestDartVersion
+
+  latestDartVersion="$(grep 'PATH' "${dvmDefaultEnv}" |
+    grep -E -o '[0-9]+\.[0-9]+\.[0-9]+')"
+
+  if ! dvm list | grep -q -o "^${latestDartVersion}\$"; then
+    err "Could not detect dart version ${latestDartVersion} installed by dvm"
+    return 1
+  fi
+
+  log_info "Adding dart version ${latestDartVersion} to PATH"
+  atp "${dvmRoot}/darts/${latestDartVersion}/bin"
+}
+
 __install_dvm() {
-  local dvmHome="${HOME}/.dvm"
+  local dvmRoot="${DVM_ROOT:-${HOME}/.dvm}"
 
   log_info "Installing DVM"
-  install_tool_from_git "dvm" "${dvmHome}" \
+  install_tool_from_git "dvm" "${dvmRoot}" \
     "https://github.com/cbracken/dvm.git"
 }
 
 __update_dvm() {
-  local dvmHome="${HOME}/.dvm"
-
-  update_tool_from_git "${dvmHome}"
+  dvm upgrade
 }
 
 # Use DVM in the current shell.
 __src_dvm_for_profile() {
-  local dvmHome="${HOME}/.dvm"
-  local dvmScript="${dvmHome}/scripts/dvm"
+  local dvmRoot="${DVM_ROOT:-${HOME}/.dvm}"
+  local dvmScript="${dvmRoot}/scripts/dvm"
 
   if [ -f "${dvmScript}" ]; then
     . "${dvmScript}"
