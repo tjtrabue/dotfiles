822ed37cc82f0c30020a4f0bfff08ff06792ab80
Tom Trabue <tom.trabue@ramseysolutions.com>
2021-12-20 16:27:15 -0600

    Added dsprune alias

--- shell/common/source/aliases/aliases_docker.sh
+++ shell/common/source/aliases/aliases_docker.sh
@@ -2,5 +2,7 @@
 
 # Remove stopped containers
 alias dcprune="docker container prune"
+# Remove unused containers, networks, and images
+alias dsprune="docker system prune"
 
 # vim:foldenable:foldmethod=marker:foldlevel=0
