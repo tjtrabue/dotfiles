9314f13c44c942cce1c2fe8c1aad272d453adf68
Tom Trabue <tom.trabue@ramseysolutions.com>
2022-01-12 15:53:02 -0600

    tiny change

--- link/config/nvim/plug-config/telescope.nvim.lua
+++ link/config/nvim/plug-config/telescope.nvim.lua
@@ -48,7 +48,7 @@ require("telescope").setup {
     buffer_previewer_maker = require "telescope.previewers".buffer_previewer_maker,
     -- Override keymappings for when Telescope is active.
     mappings = {
-      -- Insert mode mappings from inside telescope buffer
+      -- Normal mode mappings from inside telescope buffer
       n = {
         ["<C-t>"] = trouble.open_with_trouble
       },
