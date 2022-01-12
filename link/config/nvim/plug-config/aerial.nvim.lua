9474a55768e1c0b31487c7036fb5fa8a8a516297
Tom Trabue <tom.trabue@ramseysolutions.com>
2022-01-12 10:49:05 -0600

    fix aerial neovim config

--- link/config/nvim/plug-config/aerial.nvim.lua
+++ link/config/nvim/plug-config/aerial.nvim.lua
@@ -1,9 +1,22 @@
-vim.g.aerial = {
-  -- Enum: prefer_right, prefer_left, right, left
-  -- Determines the default direction to open the aerial window. The 'prefer'
-  -- options will open the window in the other direction *if* there is a
-  -- different buffer in the way of the preferred direction
-  default_direction = "prefer_left"
-}
+-- Aerial is a
+require("aerial").setup(
+  {
+    -- Enum: prefer_right, prefer_left, right, left
+    -- Determines the default direction to open the aerial window. The 'prefer'
+    -- options will open the window in the other direction *if* there is a
+    -- different buffer in the way of the preferred direction
+    default_direction = "prefer_left",
+    on_attach = function(bufnr)
+      -- Toggle the aerial window with <leader>a
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>a", "<cmd>AerialToggle!<CR>", {})
+      -- Jump forwards/backwards with '{' and '}'
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "{", "<cmd>AerialPrev<CR>", {})
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "}", "<cmd>AerialNext<CR>", {})
+      -- Jump up the tree with '[[' or ']]'
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "[[", "<cmd>AerialPrevUp<CR>", {})
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "]]", "<cmd>AerialNextUp<CR>", {})
+    end
+  }
+)
 
 -- vim:foldenable:foldmethod=marker:foldlevel=0
--- link/config/nvim/plug-config/lsp.lua
+++ link/config/nvim/plug-config/lsp.lua
@@ -43,18 +43,7 @@ local on_attach = function(client, bufnr)
   end
 
   -- Aerial code browser config. {{{
-  local aerial = require "aerial"
-  aerial.on_attach(client)
-
-  -- Aerial does not set any mappings by default, so you'll want to set some up
-  -- Toggle the aerial window with <leader>a
-  vim.api.nvim_buf_set_keymap(0, "n", "<leader>a", "<cmd>AerialToggle!<CR>", {})
-  -- Jump forwards/backwards with '{' and '}'
-  vim.api.nvim_buf_set_keymap(0, "n", "{", "<cmd>AerialPrev<CR>", {})
-  vim.api.nvim_buf_set_keymap(0, "n", "}", "<cmd>AerialNext<CR>", {})
-  -- Jump up the tree with '[[' or ']]'
-  vim.api.nvim_buf_set_keymap(0, "n", "[[", "<cmd>AerialPrevUp<CR>", {})
-  vim.api.nvim_buf_set_keymap(0, "n", "]]", "<cmd>AerialNextUp<CR>", {})
+  require("aerial").on_attach(client, bufnr)
   -- }}}
 
   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
