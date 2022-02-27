29117d55706c2f93565cc7bbe61d4fa5739a05ec
Tom Trabue <tom.trabue@gmail.com>
2022-02-26 21:47:20 -0600

    Add func to install erlang_ls

--- shell/common/source/functions/functions_erlang.sh
+++ shell/common/source/functions/functions_erlang.sh
@@ -20,4 +20,32 @@ bootstrap_erlang_project() {
   make -f "${erlangMkFile}" bootstrap bootstrap-rel
 }
 
+# Install the Erlang language server.
+install_erlang_ls() {
+  local erlangLsGitRepoUrl="https://github.com/erlang-ls/erlang_ls.git"
+  local erlangLsDest="${WS:-${HOME}/workspace}/erlang_ls"
+  # The directory containing the "bin/" directory where the erlang_ls executable
+  # script will be installed.
+  local installPrefix="${HOME}/.local"
+
+  if [ -d "${erlangLsDest}" ]; then
+    log_info "Updating erlang_ls"
+    git -C "${erlangLsDest}" clean -df
+    git -C "${erlangLsDest}" restore --staged .
+    git -C "${erlangLsDest}" restore .
+    git -C "${erlangLsDest}" pull
+  else
+    log_info "Cloning erlang_ls"
+    git clone "${erlangLsGitRepoUrl}" "${erlangLsDest}"
+  fi
+
+  log_info "Installing erlang_ls"
+  mkdir -p "${installPrefix}"
+  (
+    cd "${erlangLsDest}"
+    make
+    PREFIX="${installPrefix}" make -e install
+  )
+}
+
 # vim:foldenable:foldmethod=indent:foldnestmax=1
