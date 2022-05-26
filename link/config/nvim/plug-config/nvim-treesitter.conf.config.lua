7830bad76441e0e4501ad3b7573ab3f8cc5857b5
Tom Trabue <tom.trabue@ramseysolutions.com>
2022-05-26 16:57:58 -0500

    Fix orgmode config for Neovim

--- link/config/nvim/plug-config/nvim-treesitter.conf.config.lua
+++ link/config/nvim/plug-config/nvim-treesitter.conf.config.lua
@@ -3,25 +3,15 @@
 -- languages.
 
 require "nvim-treesitter.configs".setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   -- ignore_install = {"javascript"}, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
-    -- disable = {"c", "rust"}, -- list of language that will be disabled
+    disable = {"org"}, -- list of language that will be disabled
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = true
   }
-}
-
--- Add Org Mode module for Tree Sitter
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.org = {
-  install_info = {
-    url = "https://github.com/milisims/tree-sitter-org",
-    files = {"src/parser.c", "src/scanner.cc"}
-  },
-  filetype = "org"
 }
--- link/config/nvim/plug-config/orgmode.config.lua
+++ link/config/nvim/plug-config/orgmode.config.lua
@@ -1,5 +1,8 @@
 -- Configuration for the Org mode clone for Neovim.
 
+-- Load custom tree-sitter grammar for org filetype
+require("orgmode").setup_ts_grammar()
+
 require("orgmode").setup {
   org_agenda_files = {"~/.emacs.d/org/agenda/*"},
   org_default_notes_file = "~/.emacs.d/org/refile.org"
