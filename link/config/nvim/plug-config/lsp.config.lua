b83d879bdb19b48be3616a7b62a913ee83e84ae3
Tom Trabue <tom.trabue@gmail.com>
2022-07-12 09:17:07 -0500

    neovim: Refactor resolved_capabilities -> server_capabilities

--- link/config/nvim/plug-config/lsp.config.lua
+++ link/config/nvim/plug-config/lsp.config.lua
@@ -41,14 +41,14 @@ local on_attach = function(client, bufnr)
   buf_set_keymap("n", "<leader>lq", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
   buf_set_keymap("n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
   -- Set some keybinds conditional on server capabilities
-  if client.resolved_capabilities.document_formatting then
+  if client.server_capabilities.document_formatting then
     buf_set_keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-  elseif client.resolved_capabilities.document_range_formatting then
+  elseif client.server_capabilities.document_range_formatting then
     buf_set_keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
   end
 
   -- Set autocommands conditional on server_capabilities
-  if client.resolved_capabilities.document_highlight then
+  if client.server_capabilities.document_highlight then
     vim.api.nvim_exec(
       [[
         hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
