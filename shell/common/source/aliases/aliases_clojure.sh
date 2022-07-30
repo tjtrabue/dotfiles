1cb7572197729ba9cb2be2c6d0972c4b2cb105dc
Tom Trabue <tom.trabue@gmail.com>
2022-07-29 20:21:30 -0500

    Add clojure tool aliases

---
+++ shell/common/source/aliases/aliases_clojure.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Invoke a clojure CLI tool.
+alias cljt="clj -Ttools"
+
+# Install a clojure tool
+alias cljti="clj -Ttools install"
+alias cljtil="clj -Ttools install-latest"
+
+# vim:foldenable:foldmethod=marker:foldlevel=0
