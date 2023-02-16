80b2d3d756c0baae6c842f3d709d4b8bf7721a67
Tom Trabue <tom.trabue@gmail.com>
2023-02-16 14:09:53 -0600

    Add ubuntu update alias aptup

---
+++ shell/common/source/linux/ubuntu/aliases/aliases_ubuntu.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Update all Ubuntu packages installed with APT.
+alias aptup="sudo apt update && sudo apt full-upgrade -y"
+
+# vim:foldenable:foldmethod=indent:foldlevel=1:foldnestmax=1
