61d5dea14ef3f6bcffa20146614bb1396bbe1af3
Tom Trabue <tom.trabue@gmail.com>
2023-02-20 21:45:22 -0600

    Get rid of Ubuntu Emacs install function

--- shell/common/source/linux/ubuntu/functions/functions_ubuntu.sh
+++ shell/common/source/linux/ubuntu/functions/functions_ubuntu.sh
@@ -22,47 +22,6 @@ install_bat_ubuntu() {
   fi
 }
 
-# Intall the latest version of Emacs from source.
-#
-# If installing on Windows for WSL, see the following page for instructions:
-# https://github.com/hubisan/emacs-wsl
-#
-# As well as the following article on using Linux GUI apps on Windows:
-# https://learn.microsoft.com/en-us/windows/wsl/tutorials/gui-apps
-install_latest_emacs() {
-  local emacsGitUrl="https://github.com/emacs-mirror/emacs.git"
-  local emacsDest="${WS:-${HOME}/workspace}/emacs"
-
-  mkdir -p "$(dirname "${emacsDest}")"
-  log_info "Cloning/Updating Emacs Git Repo"
-  clone_or_update_git_repo "${emacsGitUrl}" "${emacsDest}"
-  (
-    log_info "Installing latest Emacs for Ubuntu" && \
-    cd "${emacsDest}" && \
-    ./autogen.sh && \
-    ./configure --with-json --with-tree-sitter --with-xwidgets \
-      --with-imagemagick --with-mailutils --with-native-compilation=aot \
-      --with-pgt && \
-    make -j$(nproc) && \
-    sudo make install
-  )
-}
-
-# Install the lazygit commandline Git porcelain for an Ubuntu distribution.
-install_lazygit_ubuntu() {
-  local lazygitVersion="$(curl -s \
-    "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" |
-    grep -Po '"tag_name":\s*"v\K[^"]*'
-  )"
-  (
-    log_info "Downloading latest lazygit version" && \
-    curl -sL -o lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${lazygitVersion}_Linux_x86_64.tar.gz" && \
-    tar -zxvf lazygit.tar.gz lazygit && \
-    sudo install lazygit /usr/local/bin && \
-    rm -f 'lazygit.tar.gz' 'lazygit'
-  )
-}
-
 # Install GitHub command line tools for an Ubuntu distribution.
 install_gh_tools_ubuntu() {
   type -p curl >/dev/null || sudo apt install curl -y
