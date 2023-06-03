9d0ed275096a8f62849282bca3f3e32537875e27
Tom Trabue <tom.trabue@gmail.com>
2023-06-03 12:49:42 -0500

    Create func to install custom fonts

--- shell/common/source/functions/functions_font.sh
+++ shell/common/source/functions/functions_font.sh
@@ -32,6 +32,30 @@ sync_nerd_fonts() {
   fi
 }
 
+# Install custom fonts from various locations.
+install_custom_fonts() {
+  local fontTempDir="/tmp/font"
+  local userFontsDir="${HOME}/.local/share/fonts"
+  local userFontsOtfDir="${userFontsDir}/otf"
+  local userFontsTtfDir="${userFontsDir}/ttf"
+
+  # Create user directories for OTF and TTF fonts
+  mkdir -p "${userFontsOtfDir}" "${userFontsTtfDir}"
+
+  # Install Neogrey font
+  curl -sL --create-dirs \
+    "https://dl.dafont.com/dl/?f=neogrey" -o "${fontTempDir}/neogrey/neogrey.zip"
+  unzip "${fontTempDir}/neogrey/neogrey.zip"
+  mkdir -p "${userFontsOtfDir}/Neogrey"
+  cp -v -t "${userFontsOtfDir}/Neogrey" "${fontTempDir}/neogrey/"*.otf
+
+  # Update font cache
+  fc-cache -fv
+
+  # Cleanup
+  rm -rf "${fontTempDir}"
+}
+
 __clone_fresh_nerd_fonts_repo() {
   local nerdFontsGitUrl="https://github.com/ryanoasis/nerd-fonts.git"
   local nerdFontsDestDir="${WS:-${HOME}/workspace}/nerd-fonts"
