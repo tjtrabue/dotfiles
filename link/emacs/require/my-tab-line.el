ceb921f6c7d54d05f0eb3c891cccf3f1a53a9df3
Tom Trabue <tom.trabue@gmail.com>
2023-12-26 17:33:46 -0600

    emacs: Start working on tab-line configuration

---
+++ link/emacs/require/my-tab-line.el
@@ -0,0 +1,45 @@
+;;; my-tab-line.el --- summary -*- lexical-binding: t -*-
+
+;; Author: Thomas Jacob Trabue
+;; Maintainer: Thomas Jacob Trabue
+;; Version: version
+;; Package-Requires: (dependencies)
+;; Homepage: homepage
+;; Keywords: keywords
+
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; `tab-line-mode' and `global-tab-line-mode' create visual tabs grouped per
+;; window, each one representing an open buffer.
+
+;;; Code:
+
+(require 'tab-line)
+
+;; If non-nil, calling `tab-line-switch-to-next-tab' on the last tab selects
+;; the first tab, and vice versa.
+(setq tab-line-switch-cycling t)
+
+;; Enable `tab-line-mode' globally.
+(global-tab-line-mode 1)
+
+(provide 'my-tab-line)
+
+;;; my-tab-line.el ends here
