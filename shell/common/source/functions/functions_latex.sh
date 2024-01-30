4a0618ccdf10348837868813e05db70c485ac228
Tom Trabue <tom.trabue@gmail.com>
2024-01-29 21:19:24 -0600

    Move texlive installer to script; Update mkbin template

--- shell/common/source/functions/functions_latex.sh
+++ shell/common/source/functions/functions_latex.sh
@@ -28,42 +28,4 @@ install_digestif_lsp() {
   digestif
 }
 
-# Intall the latest TeX Live distribution on a UNIX OS. This is the preferred
-# approach to installing TeX Live, since system package repositories usually
-# contain out-of-date versions that are incompatible with the latest TeX Live
-# package repositories, meaning you cannot automatically install package using
-# `tlmgr install <package>'.
-#
-# To uninstall this distribution, the cleanest solution is to use
-# `sudo tlmgr remove --all --force'. If that does not work, the directories you
-# must remove will be named with the scheme:
-#   /usr/local/texlive/YYYY
-#   ~/.texliveYYYY
-install_latest_texlive_distribution() {
-  local tempDir="/tmp"
-  local texliveDownloadUrl="https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz"
-  local tarFileName="install-tl-unx.tar.gz"
-
-  log_info "Installing latest TeX Live distribution"
-  (
-    cd "${tempDir}" && \
-    wget "${texliveDownloadUrl}" && \
-    zcat "${tarFileName}" | tar xf - && \
-    (
-      cd "$(find "${tempDir}" -maxdepth 1 -mindepth 1 \
-        -type d -name 'install-tl-*')" && \
-      sudo perl ./install-tl --no-interaction
-    )
-  ) && \
-  rm -rf "${tempDir:?}"/install-tl-* && \
-  cat <<EOF
-Done! Do not forget to prepend this to your PATH:
-  /usr/local/texlive/YYYY/bin/PLATFORM,
-You will also want to add these to your mandb config file (/etc/manpath.config):
-  /usr/local/texlive/YYYY/texmf-dist/doc/man
-and your INFOPATH file (~/.info_path):
-  /usr/local/texlive/YYYY/texmf-dist/doc/info
-EOF
-}
-
 # vim:foldenable:foldmethod=indent:foldnestmax=1
