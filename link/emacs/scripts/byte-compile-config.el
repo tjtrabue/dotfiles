d07ea0bbe770e5eb4e83024c37b115c72fd1b9ab
Tom Trabue <tom.trabue@gmail.com>
2024-02-08 13:33:17 -0600

    emacs: Still working on proper byte-compiling

--- link/emacs/scripts/byte-compile-config.el
+++ link/emacs/scripts/byte-compile-config.el
@@ -1,8 +1,9 @@
 ;; Add `/usr/local/share/emacs/site-lisp/' to load-path, and then add all of its
 ;; child directories recursively to load-path
-(let ((usr-local-lisp "/usr/local/share/emacs/site-lisp")
-       (straight-build-dir (file-truename (concat user-emacs-directory "straight/build")))
-       (super-config-dir (file-truename (concat user-emacs-directory "super_config"))))
+(let* ((usr-local-lisp "/usr/local/share/emacs/site-lisp")
+        (straight-dir (file-truename (concat user-emacs-directory "straight")))
+        (straight-build-dir (file-truename (concat straight-dir "/build")))
+        (super-config-dir (file-truename (concat user-emacs-directory "super_config"))))
   (when (file-directory-p usr-local-lisp)
     (let ((default-directory usr-local-lisp))
       (add-to-list 'load-path usr-local-lisp)
@@ -17,5 +18,11 @@
   ;; Add all subdirectories of `~/.emacs.d/straight/build' to `load-path'.
   (dolist (dir (directory-files straight-build-dir 'full-name))
     (add-to-list 'load-path dir))
+
+  (require 'bytecomp)
+  (require 'use-package)
+  (load (file-truename (concat straight-dir "/repos/straight.el/bootstrap.el")))
+  (require 'straight)
+  (require 'general)
 
   (byte-recompile-directory super-config-dir 0))
