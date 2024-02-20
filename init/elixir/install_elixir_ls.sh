e3db27525df535ecb8b8fc9b3821119a6e67a4f3
Tom Trabue <tom.trabue@gmail.com>
2024-02-19 22:00:35 -0600

    Move elixir-ls installer to script
    Also use elixir-ls instead of lexical in Emacs for now.
    elixir-ls is more mature, faster, has better snippet support, and is
    overall much more intelligent. Lexical will get there one of these days,
    though. I have no doubt of that.

---
+++ init/elixir/install_elixir_ls.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+
+# Trap errors and print error message
+set -uo pipefail
+trap 's=$?; echo "$0: Error on line "${LINENO}": ${BASH_COMMAND}"; exit $s' ERR
+
+# Variables {{{
+declare DOTFILES_HOME="${HOME}/.dotfiles"
+declare THIS_EXEC="$(basename "${BASH_SOURCE[0]}")"
+declare WS="${WS:-${HOME}/workspace}"
+declare INSTALL_DIR="${WS}/elixir-ls"
+declare ELIXIR_LS_RELEASE_DIR="${INSTALL_DIR}/release"
+
+# Logging variables
+declare LOG_TO_FILE=""
+declare LOG_LEVEL=3
+# }}}
+
+# Imports {{{
+. "${DOTFILES_HOME}/shell/common/source/common.sh" && src
+# }}}
+
+# Functions {{{
+_help() {
+  command cat <<EOF
+${THIS_EXEC} | Install elixir-ls server
+
+Once installed, and the release directory has been generated, make sure to add
+the release directory to the \$PATH environment variable.
+
+USAGE:
+  ${THIS_EXEC} [OPTIONS]
+
+OPTIONS:
+  -h | --help
+    Print the help message (this message) and exit.
+EOF
+}
+
+# Clone and build elixir-ls.
+iels__install_elixir_ls() {
+  local gitUrl="https://github.com/elixir-lsp/elixir-ls.git"
+
+  clone_or_update_git_repo "${gitUrl}" "${INSTALL_DIR}"
+  (
+    log_info "Installing elixir-ls" &&
+      cd "${INSTALL_DIR}" &&
+      mix deps.get &&
+      mix clean &&
+      rm -rf "${INSTALL_DIR}/_build" "${ELIXIR_LS_RELEASE_DIR}" &&
+      MIX_ENV=prod mix compile &&
+      MIX_ENV=prod mix elixir_ls.release2 -o "${ELIXIR_LS_RELEASE_DIR}"
+  )
+}
+
+# Add the release dir to PATH so that the executables are discoverable.
+iels__add_elixir_ls_release_dir_to_path() {
+  if [ -d "${ELIXIR_LS_RELEASE_DIR}" ]; then
+    atp "${ELIXIR_LS_RELEASE_DIR}"
+  else
+    err "Could not find elixir-ls release dir at:" \
+      "${BLUE}${ELIXIR_LS_RELEASE_DIR}${NC}"
+    return 1
+  fi
+}
+
+iels__main() {
+  print_header "Installing elixir-ls"
+  iels__install_elixir_ls
+  iels__add_elixir_ls_release_dir_to_path
+}
+# }}}
+
+# Parse CLI Options {{{
+args=$(getopt -o h --long help -n 'install_elixir_ls.sh' -- "$@")
+eval set -- "$args"
+
+# extract options and their arguments into variables.
+while true; do
+  case "$1" in
+  -h | --help)
+    _help
+    exit 0
+    ;;
+
+  --)
+    shift
+    break
+    ;;
+
+  *)
+    err "Unknown option $1 to ${THIS_EXEC}"
+    exit 2
+    ;;
+  esac
+done
+# }}}
+
+iels__main "${@}"
+
+# vim:foldenable:foldmethod=marker:
