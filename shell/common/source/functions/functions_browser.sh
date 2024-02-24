ebbc4b7591d3654b4417d5a18537f59d2cc188c7
Tom Trabue <tom.trabue@gmail.com>
2024-02-24 13:56:22 -0600

    emacs: Add link to polymode article in config

--- shell/common/source/functions/functions_browser.sh
+++ shell/common/source/functions/functions_browser.sh
@@ -3,6 +3,11 @@
 # Launch Brave browser with additional features enabled.
 brave() {
   braveExec="$(command -v brave-beta)"
+
+  if [ -z "${braveExec}" ]; then
+    braveExec="$(command -v brave)"
+  fi
+
   braveFlags="--enable-accelerated-2d-canvas --enable-gpu-rasterization --enable-skia-graphite"
 
   if [ ! -x "${braveExec}" ]; then
