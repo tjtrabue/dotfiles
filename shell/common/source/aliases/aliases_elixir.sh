91e9bd9bedb2b621162fb9264b5f52af6dc234db
Tom Trabue <tom.trabue@gmail.com>
2024-04-19 10:23:19 -0500

    Add pmix alias -> prod mix

--- shell/common/source/aliases/aliases_elixir.sh
+++ shell/common/source/aliases/aliases_elixir.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-# Start iex and load the current project's modules.
+# Start `iex` and load the current project's modules.
 alias iexm="iex -S mix"
 
-# Start Phoenix server interactively in iex.
+# Start Phoenix server interactively in `iex`.
 alias iphx="iex -S mix phx.server"
+
+# Run `mix` in the prod environment.
+alias pmix="MIX_ENV=prod mix"
 
 # vim:foldenable:foldmethod=marker:foldlevel=0
