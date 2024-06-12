ab4b26aaaf0768fad11e8da1a2904dda20712eb5
Tom Trabue <tom.trabue@gmail.com>
2024-06-11 19:26:26 -0500

    emacs: Fix my-tab-line-mode activation
    Fixed the problem of the custom tab function not being passed along to
    `tab-line-tabs-function`.

--- link/emacs/require/my-tab-line.el
+++ link/emacs/require/my-tab-line.el
@@ -50,6 +50,12 @@
   :group 'my-tab-line)
 
 ;;;###autoload
+(defcustom my-tab-line-tabs-function #'my-tab-line-default-tabs-function
+  "Specifies the function used to filter tabs for display."
+  :type '(function)
+  :group 'my-tab-line)
+
+;;;###autoload
 (defgroup my-tab-line '((my-tab-line-allowed-regexps custom-variable)
                          (my-tab-line-allowed-major-modes custom-variable))
   "My special `tab-line' variables.")
@@ -57,6 +63,10 @@
 (defun my-tab-line-tab-name-function (buffer &optional _buffers)
   "Print tab name for BUFFER with some leading space for readability."
   (concat " " (buffer-name buffer) " "))
+
+(defun my-tab-line-default-tabs-function ()
+  "Displays buffers in the `tab-line'."
+  (my-tab-line-filter-display-buffers (buffer-list)))
 
 (defun my-tab-line-filter-display-buffers (bufs)
   "Determine which buffers in BUFS will show up in `tab-line'.
@@ -98,29 +108,10 @@ configuration in `my-icons.org' for more details."
   (progn
     (require 'tab-line)
 
-    ;; Specify a function to determine the list of buffers to display in the
-    ;; `tab-line'.
-    (setq tab-line-tabs-function (lambda ()
-                                   ;; Default to apply filtering to the entire
-                                   ;; `buffer-list'.
-                                   (my-tab-line-filter-display-buffers (buffer-list))))
-    ;; If non-nil, calling `tab-line-switch-to-next-tab' on the last tab selects
-    ;; the first tab, and vice versa.
-    (setq tab-line-switch-cycling t)
-    ;; Specify the function used to print the tab name in the tab line.
-    (setq tab-line-tab-name-function #'my-tab-line-tab-name-function)
-    ;; Whether to show the "x" close button next to each tab.
-    (setq tab-line-close-button-show nil)
-    ;; Whether to show the "+" button used to add a new tab.
-    (setq tab-line-new-button-show nil)
-    ;; This is a custom callback function that the user can define which decides
-    ;; into which tab group an input buffer gets added.
-    ;;
-    ;; This function takes a buffer as an argument, and should return a string
-    ;; representing the name of the group into which the input buffer should be
-    ;; placed. If the function returns nil, the buffer will be filtered out
-    ;; entirely.
-    (setq tab-line-tabs-buffer-group-function nil)
+    ;; Whenever the user customizes `my-tab-line-tabs-function', pass the
+    ;; customization along to `tab-line-tabs-function'.
+    (add-variable-watcher 'my-tab-line-tabs-function (lambda (_symbol newval _operation _where)
+                                                       (setq tab-line-tabs-function newval)))
 
     ;; Adjust tab-line faces.
     (dolist (face '(tab-line
@@ -133,8 +124,33 @@ configuration in `my-icons.org' for more details."
         :width 'expanded
         ;; Increase tab-line height by adding a border box.
         :box `(:line-width 4 :color ,(face-background face nil t))))
-    ;; Enable `tab-line-mode' globally.
-    (global-tab-line-mode 1)))
+
+    (if my-tab-line-mode
+      (progn
+        ;; If non-nil, calling `tab-line-switch-to-next-tab' on the last tab selects
+        ;; the first tab, and vice versa.
+        (setq tab-line-switch-cycling t)
+        ;; Specify the function used to print the tab name in the tab line.
+        (setq tab-line-tab-name-function #'my-tab-line-tab-name-function)
+        ;; Whether to show the "x" close button next to each tab.
+        (setq tab-line-close-button-show nil)
+        ;; Whether to show the "+" button used to add a new tab.
+        (setq tab-line-new-button-show nil)
+        ;; This is a custom callback function that the user can define which decides
+        ;; into which tab group an input buffer gets added.
+        ;;
+        ;; This function takes a buffer as an argument, and should return a string
+        ;; representing the name of the group into which the input buffer should be
+        ;; placed. If the function returns nil, the buffer will be filtered out
+        ;; entirely.
+        (setq tab-line-tabs-buffer-group-function nil)
+        ;; Specify a function to determine the list of buffers to display in the
+        ;; `tab-line'.
+        (setq tab-line-tabs-function my-tab-line-tabs-function)
+        ;; Turn on the tab-line globally.
+        (global-tab-line-mode 1))
+      ;; Deactivate the tab-line when the user toggles this minor-mode.
+      (global-tab-line-mode -1))))
 
 (provide 'my-tab-line)
 
