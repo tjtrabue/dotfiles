29663a0721f56b4326ce9f77961db223e7445f0b
Tom Trabue <tom.trabue@gmail.com>
2024-10-31 21:58:41 -0500

    Add guard blocks to font installer logic
    Also add CJK compatibility to unicode-fonts

--- shell/common/source/functions/functions_font.sh
+++ shell/common/source/functions/functions_font.sh
@@ -43,34 +43,54 @@ install_custom_fonts() {
   mkdir -p "${userFontsOtfDir}" "${userFontsTtfDir}"
 
   # Install Neogrey font
-  curl -sL --create-dirs \
-    "https://dl.dafont.com/dl/?f=neogrey" -o "${fontTempDir}/neogrey/neogrey.zip"
-  unzip "${fontTempDir}/neogrey/neogrey.zip"
-  mkdir -p "${userFontsOtfDir}/Neogrey"
-  cp -v -t "${userFontsOtfDir}/Neogrey" "${fontTempDir}/neogrey/"*.otf
+  if [ ! -d "${userFontsOtfDir}/Neogrey" ]; then
+    curl -sL --create-dirs \
+      "https://dl.dafont.com/dl/?f=neogrey" -o "${fontTempDir}/neogrey/neogrey.zip"
+    unzip "${fontTempDir}/neogrey/neogrey.zip"
+    mkdir -p "${userFontsOtfDir}/Neogrey"
+    cp -v -t "${userFontsOtfDir}/Neogrey" "${fontTempDir}/neogrey/"*.otf
+  else
+    log_info "Neogrey font directory already found at:" \
+      "${BLUE}${userFontsOtfDir}/Neogrey${NC}"
+  fi
 
   # Install Arkibal Serif font
-  curl -sL --create-dirs \
-    "https://allbestfonts.com/wp-content/uploads/2017/12/elements-arkibal-serif-58UCG6-2017-03-03.zip" \
-    -o "${fontTempDir}/arkibal_serif/arkibal_serif.zip"
-  unzip "${fontTempDir}/arkibal_serif/arkibal_serif.zip"
-  mkdir -p "${userFontsOtfDir}/ArkibalSerif" "${userFontsTtfDir}/ArkibalSerif"
-  cp -v -t "${userFontsOtfDir}/ArkibalSerif" "${fontTempDir}/arkibal_serif/"*.otf
-  cp -v -t "${userFontsTtfDir}/ArkibalSerif" "${fontTempDir}/arkibal_serif/"*.ttf
+  if [ ! -d "${userFontsOtfDir}/ArkibalSerif" ]; then
+    curl -sL --create-dirs \
+      "https://allbestfonts.com/wp-content/uploads/2017/12/elements-arkibal-serif-58UCG6-2017-03-03.zip" \
+      -o "${fontTempDir}/arkibal_serif/arkibal_serif.zip"
+    unzip "${fontTempDir}/arkibal_serif/arkibal_serif.zip"
+    mkdir -p "${userFontsOtfDir}/ArkibalSerif" "${userFontsTtfDir}/ArkibalSerif"
+    cp -v -t "${userFontsOtfDir}/ArkibalSerif" "${fontTempDir}/arkibal_serif/"*.otf
+    cp -v -t "${userFontsTtfDir}/ArkibalSerif" "${fontTempDir}/arkibal_serif/"*.ttf
+  else
+    log_info "Arkibal Serif font directory already found at:" \
+      "${BLUE}${userFontsOtfDir}/ArkibalSerif${NC}"
+  fi
 
   # Install Quivira font
-  mkdir -p "${userFontsOtfDir}/Quivira"
-  wget -qP "${userFontsOtfDir}/Quivira" "http://quivira-font.com/files/Quivira.otf"
+  if [ ! -d "${userFontsOtfDir}/Quivira" ]; then
+    mkdir -p "${userFontsOtfDir}/Quivira"
+    wget -qP "${userFontsOtfDir}/Quivira" "http://quivira-font.com/files/Quivira.otf"
+  else
+    log_info "Quivira font directory already found at:" \
+      "${BLUE}${userFontsOtfDir}/Quivira${NC}"
+  fi
 
   # Install Symbola font
-  mkdir -p "${userFontsTtfDir}/Symbola" "${fontTempDir}/symbola"
-  wget -qP "${fontTempDir}" \
-    "https://fontlibrary.org/assets/downloads/symbola/cf81aeb303c13ce765877d31571dc5c7/symbola.zip"
-  unzip "${fontTempDir}/symbola/symbola.zip"
-  cp -v -t "${userFontsTtfDir}/Symbola" "${fontTempDir}/symbola/"*.ttf
+  if [ ! -d "${userFontsTtfDir}/Symbola" ]; then
+    mkdir -p "${userFontsTtfDir}/Symbola" "${fontTempDir}/symbola"
+    wget -qP "${fontTempDir}" \
+      "https://fontlibrary.org/assets/downloads/symbola/cf81aeb303c13ce765877d31571dc5c7/symbola.zip"
+    unzip "${fontTempDir}/symbola/symbola.zip"
+    cp -v -t "${userFontsTtfDir}/Symbola" "${fontTempDir}/symbola/"*.ttf
+  else
+    log_info "Symbola font directory already found at:" \
+      "${BLUE}${userFontsTtfDir}/Symbola${NC}"
+  fi
 
   # Update font cache
-  fc-cache -fv
+  fc-cache -rfv
 
   # Cleanup
   rm -rf "${fontTempDir}"
