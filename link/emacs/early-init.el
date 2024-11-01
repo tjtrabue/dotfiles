972c40cd7fe8253ba74d298ac0a1879984f9fcf0
Tom Trabue <tom.trabue@gmail.com>
2024-11-01 17:32:58 -0500

    emacs: Add early-init.el file for frame initialization

---
+++ link/emacs/early-init.el
@@ -0,0 +1,24 @@
+;;; The very first configuration file, read even before `~/.emacs'.
+;; Here we set values that must be defined before the first GUI frame is created.
+
+;; * Fullscreen options
+;; To change the initial fullscreen behavior of a frame using =initial-frame-alist=
+;; or =default-frame-alist=, append one of the following options to one or both of
+;; those lists:
+;;
+;; - `(fullscreen . fullwidth)': Make the frame as wide as possible, but do not
+;; adjust vertical size.
+;; - `(fullscreen . fullheight)': Make the frame as tall as possible, but do not
+;; adjust horizontal size.
+;; - `(fullscreen . fullboth)': Set height and width to the size of the screen.
+;; - `(fullscreen . maximized)': Like =fullboth=, but you cannot readjust the
+;; frame size later with the mouse.
+;;
+;; ** How to adjust the initial frame's size
+;; Use the `initial-frame-alist' to change the size of the first frame Emacs
+;; creates on startup.
+;;
+;; ** How to adjust all frames' sizes
+;; To change the size of all frames Emacs creates, set `default-frame-alist', instead.
+;; Maximize Emacs' initial frame.
+(add-to-list 'initial-frame-alist `(fullscreen . fullboth))
