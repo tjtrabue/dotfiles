42878846cf4d175e9e50f7eb53e41c260c129f66
Tom Trabue <tom.trabue@gmail.com>
2024-11-01 22:06:35 -0500

    emacs: Tweak unicode fonts config

--- link/emacs/require/my-font.el
+++ link/emacs/require/my-font.el
@@ -46,6 +46,7 @@
          (satoshi-plist '(:family "Satoshi Variable" :height 110 :weight medium :width normal))
          ;; Default fonts
          (cascadia-code-plist '(:family "Cascadia Code PL" :height 110 :weight semi-bold :width normal))
+         (jetbrains-mono-plist '(:family "JetBrainsMono Nerd Font" :height 110 :weight semi-bold :width normal))
          (fira-code-plist '(:family "FiraCode Nerd Font" :height 110 :weight semi-bold :width normal))
          (noto-sans-plist '(:family "Noto Sans" :height 110 :weight semi-bold :width normal))
          (dejavu-sans-plist '(:family "DejaVu Sans Mono" :height 110 :weight semi-bold :width normal)))
@@ -54,6 +55,8 @@
         (cond
           ((member (plist-get cascadia-code-plist :family) font-family-list)
             cascadia-code-plist)
+          ((member (plist-get jetbrains-mono-plist :family) font-family-list)
+            jetbrains-mono-plist)
           ((member (plist-get fira-code-plist :family) font-family-list)
             fira-code-plist)
           (t dejavu-sans-plist)))
