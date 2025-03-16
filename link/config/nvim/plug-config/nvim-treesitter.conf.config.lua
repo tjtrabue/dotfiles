0df93bec35c6d6878c489b3a17ef7c0c678627a6
Tom Trabue <tom.trabue@gmail.com>
2025-03-15 21:04:50 -0500

    nvim: Ignore orgmode for treesitter grammar

--- link/config/nvim/plug-config/nvim-treesitter.conf.config.lua
+++ link/config/nvim/plug-config/nvim-treesitter.conf.config.lua
@@ -4,7 +4,7 @@
 
 require "nvim-treesitter.configs".setup {
   ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  -- ignore_install = {"javascript"}, -- List of parsers to ignore installing
+  ignore_install = {"org"}, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
     -- disable = {"org"}, -- list of language that will be disabled
