ded5f839a8f4028e96cc6ff762854cef20e17172
Tom Trabue <tom.trabue@gmail.com>
2025-03-29 23:21:09 -0500

    Add jctl alias for getting journalctl errors

--- shell/common/source/aliases/aliases_linux.sh
+++ shell/common/source/aliases/aliases_linux.sh
@@ -3,5 +3,8 @@
 # Start terminator with a pre-defined layout
 alias term="src && terminator -l coding"
 
+# Convenience alias for locating errors in journalctl since the last boot.
+alias jctl="journalctl -p 3 -xb"
+
 # Modeline for this file (LEAVE IT COMMENTED!!!)
 # vim:foldenable:foldmethod=marker
