1a6014271ed7f964534e74c878db601f85c4a08f
Tom Trabue <tom.trabue@gmail.com>
2025-03-30 11:37:55 -0500

    arch: Add alias for inxi -eazy

--- shell/common/source/aliases/aliases_linux.sh
+++ shell/common/source/aliases/aliases_linux.sh
@@ -6,5 +6,10 @@ alias term="src && terminator -l coding"
 # Convenience alias for locating errors in journalctl since the last boot.
 alias jctl="journalctl -p 3 -xb"
 
+# Shortcut to output a host of useful system information, including system
+# hardware, CPU, drivers, Xorg, Desktop, Kernel, compiler versions, Processes,
+# RAM usage, swap info, and much more.
+alias sysinfo="inxi -eazy"
+
 # Modeline for this file (LEAVE IT COMMENTED!!!)
 # vim:foldenable:foldmethod=marker
